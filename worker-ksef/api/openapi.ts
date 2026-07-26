2bfd5709132c32088515cc3fc9bbbcfda8b206da
Diego Ruiz Barbero <drexa1@hotmail.com>
2026-07-26 18:26:36 +0200

    Update openapi.ts

--- worker-ksef/api/openapi.ts
+++ worker-ksef/api/openapi.ts
@@ -166,3 +166,30 @@ export const openApiSpec = {
         }
     }
 };
+
+export const swaggerHtml = `
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<title>API docs</title>
+			<link
+				rel="stylesheet"
+				href="https://unpkg.com/swagger-ui-dist/swagger-ui.css"
+			/>
+		</head>
+
+		<body>
+			<div id="swagger-ui"></div>
+
+			<script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+
+			<script>
+				// noinspection JSUnresolvedVariable
+				SwaggerUIBundle({
+					url: "/openapi.json",
+					dom_id: "#swagger-ui",
+				});
+			</script>
+		</body>
+	</html>
+`;
