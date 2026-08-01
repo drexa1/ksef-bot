bba51ee8c6cf3aa72336aa316602ec13769cfd5c
drexa1 <drexa1@hotmail.com>
2026-08-01 19:07:40 +0200

    Rework auth

--- worker/routes/ksef/ksef.purchases.ts
+++ worker/routes/ksef/ksef.purchases.ts
@@ -1,4 +1,4 @@
-import {Env} from "../worker";
+import {Env} from "../../worker";
 
 export async function get(req: Request, env: Env): Promise<Response> {
     const url = new URL(req.url);
@@ -6,9 +6,4 @@ export async function get(req: Request, env: Env): Promise<Response> {
     const searchResult: any[] = [];  // TODO
     if (!searchResult.length) return Response.json("Invoice not found", { status: 404 });
     return Response.json(searchResult);
-}
-
-export async function post(req: Request, env: Env): Promise<Response> {
-    // TODO
-    return Response.json(null);
 }
