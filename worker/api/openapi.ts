eb6f19379c33625869e479915079faa0a56e1ee0
drexa1 <drexa1@hotmail.com>
2026-08-01 22:05:02 +0200

    Rework auth

--- worker/api/openapi.ts
+++ worker/api/openapi.ts
@@ -70,6 +70,7 @@ export const openApiSpec = {
                 type: "object",
                 additionalProperties: false,
                 required: [
+                    "period",
                     "brut_income"
                 ],
                 properties: {
