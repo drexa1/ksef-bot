f5cc5e3923243b087115d56fd08b270c1af388b1
drexa1 <drexa1@hotmail.com>
2026-08-10 18:11:30 +0200

    PII

--- .github/workflows/api-tests-ci.sh
+++ .github/workflows/api-tests-ci.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
-
 set -e
 
+echo "Recreating local database..."
 npm run db:local:recreate
 
-echo "Starting Worker..."
+echo "Starting worker..."
 npm run worker:dev > worker.log 2>&1 &
 WORKER_PID=$!
 
 cleanup() {
-    echo "Stopping Worker..."
+    echo "Stopping worker..."
     kill "$WORKER_PID" 2>/dev/null || true
     wait "$WORKER_PID" 2>/dev/null || true
 }
 
 trap cleanup EXIT
-
 echo "Waiting for Worker..."
 npx wait-on http://127.0.0.1:3001
 
