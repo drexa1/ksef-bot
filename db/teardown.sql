30990561956879742205e3a7202ad402126cf0e7
drexa1 <drexa1@hotmail.com>
2026-08-19 20:25:03 +0200

    Invoice generator frontend

--- db/teardown.sql
+++ db/teardown.sql
@@ -1,7 +1,7 @@
 PRAGMA foreign_keys = OFF;
 
-DROP TABLE IF EXISTS taxes;
 DROP TABLE IF EXISTS invoices;
+DROP TABLE IF EXISTS taxes;
 DROP TABLE IF EXISTS counterparties;
 DROP TABLE IF EXISTS users;
 
